9845602184ab26fc21a68e66b8b13ea21320c814
RL Brown <rlbrown72@gmail.com>
2018-04-15 12:11:55 -0400

    testing indexedDB pop in index.js not sw

--- src/index.js
+++ src/index.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { set } from 'idb-keyval';
+
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import howard from './data/howard.json';
+
+const createDB = () => {
+  set('quotes', howard);
+};
+
+createDB();
 
 ReactDOM.render(<App />, document.getElementById('root')); // eslint-disable-line
 registerServiceWorker();
