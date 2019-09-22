e6a0d9dd93a05c29a10c1b324fcf2f94c401d9ef
RL Brown <rlbrown72@gmail.com>
2019-09-22 14:25:53 -0400

    new sw issue

--- sw-custom.js
+++ sw-custom.js
@@ -2,3 +2,12 @@ if (t.request.url.indexOf('/oauth') !== -1) return false;
 
 // FUTURE SELF: you're keeping the serviceworker but going around it for this one route
 // insert it into the sw
+
+// Sept 2019 -- but now we have Workbox so maybe this:
+
+// am i supposed to put this in the build by-hand? like i was with the above??
+
+// workbox.routing.registerRoute(
+//   new RegExp('/oauth/'),
+//   new workbox.strategies.NetworkOnly()
+// );
